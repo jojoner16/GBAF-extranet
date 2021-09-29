0944e866f324986cf28be484f8129da571bb229b
Laybats Jonathan <laybats.jonathan@orange.fr>
2021-09-29 11:44:06 +0200

    modification de connexion et ajout header/footer

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     require_once 'php/fonction.php';
     $pdo = connect_bdd();
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
     <head>
@@ -11,99 +12,69 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/jpg" href="images/logo.png" />
-        <link rel="stylesheet" type="text/css" href="css/styles.css">
-        <title>GBAF</title>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <title>GBAF connexion</title>
     </head>
     
     <body>
         <?php
+            require_once 'header-footer/header.php';
+            // Si l'action "déconnexion" est passé en URL, on détruit la session
+			if(isset($_GET['action']) && $_GET['action'] === 'deconnexion')
+			{
+				$_SESSION['message']= 'Vous venez de vous déconnecter.';
+				header('Refresh: 2; url=connexion.php');
+				session_destroy();
 
-            // REDIRECTION: CONNECTÉ
-            if (isset($_SESSION['nom']) && isset($_SESSION['prenom']) && isset($_SESSION['id_user'])) 
-            {
-                header('Location: user_conected.php');
-                exit();
-            }
+				// Suppression des cookies de connexion automatique
+				setcookie('username', '');
+				setcookie('pasword', '');
+			}
 
-            if (isset($_POST['dataSubmit']) && !empty($_POST['username']) && !empty($_POST['password'])) 
-            {
-                // Cherche l'utilisateur dans la BDD (voir account)
-                $dataAccount = searchUser($bdd, $_POST['username']);
-                // Si l'user existe, et que tous les champs sont remplis
-                if ($dataAccount) 
-                {
-                    // Vérification mot de passe (Hashé)
-                    $isPasswordCorrect = password_verify($_POST['password'], $dataAccount['password']);
-                    // Si le mot de passe correspond
-                    if ($isPasswordCorrect) 
-                    {
-                        // Connexion
-                        $_SESSION['nom'] = htmlspecialchars($dataAccount['nom']);
-                        $_SESSION['prenom'] = htmlspecialchars($dataAccount['prenom']);
-                        $_SESSION['id_user'] = $dataAccount['id_user'];
-                        $_SESSION['username'] = htmlspecialchars($dataAccount['username']);
+            if(isset($_POST['username']) && isset($_POST['password'])) 
+            {   
+                $require= $pdo->prepare('SELECT username, password FROM user WHERE id_user');
 
-                        header('Location: user_conected.php');
-                        exit();
-                    }
-                    //mot de passe incorrect
-                    if (!$isPasswordCorrect) 
-                    {
-                        $message = MOT_DE_PASSE_INCORRECT;
-                    }
-                }
-                // identifiant n'existe pas
-                if (!$dataAccount) 
-                {
-                    $message = UTILISATEUR_INCONNU;
-                }
-            }
-            // champs non remplis
-            if (isset($_POST['dataSubmit']) && empty($_POST['username']) && empty($_POST['password'])) 
-            {
-                $message = CHAMP_VIDE;
-            }
+                $username= htmlspecialchars($_POST['username']);
+                $password= htmlspecialchars($_POST['password']);
 
-
-
-            // NON CONNECTÉ - page de connexion
-            if (!isset($_SESSION['nom']) && !isset($_SESSION['prenom']) && !isset($_SESSION['id_user'])) 
-            {
-                require_once('member_area.php');
-
+                $require->execute(array(
+                    ':username'=> $username,
+                    ':password'=> $password
+                ));
+                $user = $require->fetch(PDO::FETCH_ASSOC);
+                   
+                $_SESSION['id_user'] = $user['id_user'];
+                $_SESSION['username'] = htmlspecialchars($POST['username']);
+                $_SESSION['nom'] = htmlspecialchars($dataAccount['nom']);
+                $_SESSION['prenom'] = htmlspecialchars($dataAccount['prenom']);
+                header('Location: espace-membre/accueil.php');   
+            }            
         ?>
 
+                    <!-- index html -->
     <main class="inscription-connexion">
         <div class="form_container">
             <fieldset>
                 <legend>Se connecter :</legend>
+                    <form method="post" action="index.php">
+                        <p>
+                            <label for="pseudo">Identifiant : </label>
+                            <input type="text" id="pseudo" name="username" required/>
 
-                <span class="message">
-                    <?php echo $message; ?>
-                </span>
-                <form method="post" action="index.php">
-                    <p>
-                        <label for="pseudo">Identifiant : </label>
-                        <input type="text" id="pseudo" name="username" required/>
+                            <label for="mp">Mot de passe : </label>
+                            <input type="password" id="mp" name="password" required/>
 
-                        <label for="mp">Mot de passe : </label>
-                        <input type="password" id="mp" name="password" required/>
-
-                        <input class="button-envoyer" type="submit" name="dataSubmit" value="Connexion" onclick=" unsetPreviousSession()"/>
-                                    
-                        <span>Les champs indiqués par une <em>*</em> sont obligatoires</span>    
-                    </p>
-                </form>
-                <a href="/mp.php"> mot de passe oublié ? </a>
-
-                <a href="/inscription.php"> créer un compte </a>
+                            <input class="button-envoyer" type="submit" name="dataSubmit" value="Connexion"/>
+                                        
+                            <span>Les champs indiqués par une <em>*</em> sont obligatoires</span>    
+                        </p>
+                    </form>
+                <a href="mp.php"> mot de passe oublié ? </a>
+                <a href="inscription.php"> créer un compte </a>
             </fieldset>
         </div>
     </main>
-
-    <?php 
-        // require_once('pied_de_page.php');
-    }
-    ?>
+        <?php require_once 'header-footer/footer.php'?>
     </body>
 </html>
