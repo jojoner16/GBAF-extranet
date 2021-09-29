9b2bd11648cc855079d5608d6b210dba083f4dc6
Laybats Jonathan <laybats.jonathan@orange.fr>
2021-09-29 16:35:54 +0200

    modifications requette et messages erreur

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-    session_start();
+    //session_start();
     // Chargement des fonctions génériques
     require_once 'php/fonction.php';
     $pdo = connect_bdd();
@@ -19,37 +19,54 @@
     <body>
         <?php
             require_once 'header-footer/header.php';
-            // Si l'action "déconnexion" est passé en URL, on détruit la session
-			if(isset($_GET['action']) && $_GET['action'] === 'deconnexion')
-			{
-				$_SESSION['message']= 'Vous venez de vous déconnecter.';
-				header('Refresh: 2; url=connexion.php');
-				session_destroy();
+            require_once 'php/account.php';
 
-				// Suppression des cookies de connexion automatique
-				setcookie('username', '');
-				setcookie('pasword', '');
-			}
+            // REDIRECTION: CONNECTÉ
+            if (isset($_SESSION['nom']) && isset($_SESSION['prenom']) && isset($_SESSION['id_user'])) 
+            {
+                header('Location: espace-membre/accueil.php');
+                exit();
+            }
+            if (isset($_POST['dataSubmit']) && !empty($_POST['username']) && !empty($_POST['password'])) 
+            {
+                // Cherche l'utilisateur dans la BDD (voir account)
+                $dataAccount = searchUser($pdo, $_POST['username']);
+                // Si l'user existe, et que tous les champs sont remplis
+                if ($dataAccount) 
+                {
+                    // Vérification mot de passe (Hashé)
+                    $isPasswordCorrect = password_verify($_POST['password'], $dataAccount['password']);
+                    // Si le mot de passe correspond
+                    if ($isPasswordCorrect) 
+                    {
+                        // Connexion
+                        $_SESSION['nom'] = htmlspecialchars($dataAccount['nom']);
+                        $_SESSION['prenom'] = htmlspecialchars($dataAccount['prenom']);
+                        $_SESSION['id_user'] = $dataAccount['id_user'];
+                        $_SESSION['username'] = htmlspecialchars($dataAccount['username']);
 
-            if(isset($_POST['username']) && isset($_POST['password'])) 
-            {   
-                $require= $pdo->prepare('SELECT username, password FROM user WHERE id_user');
-
-                $username= htmlspecialchars($_POST['username']);
-                $password= htmlspecialchars($_POST['password']);
-
-                $require->execute(array(
-                    ':username'=> $username,
-                    ':password'=> $password
-                ));
-                $user = $require->fetch(PDO::FETCH_ASSOC);
-                   
-                $_SESSION['id_user'] = $user['id_user'];
-                $_SESSION['username'] = htmlspecialchars($POST['username']);
-                $_SESSION['nom'] = htmlspecialchars($dataAccount['nom']);
-                $_SESSION['prenom'] = htmlspecialchars($dataAccount['prenom']);
-                header('Location: espace-membre/accueil.php');   
-            }            
+                        header('Location: espace-membre/accueil.php');
+                        exit();
+                    }
+                    //mot de passe incorrect
+                    if (!$isPasswordCorrect) 
+                    {
+                        $message = PASSWORD_WRONG;
+                    }
+                }
+                // identifiant n'existe pas
+                if (!$dataAccount) 
+                {
+                    $message = USERNAME_UNKNOWN;
+                }
+            }
+            // champs non remplis
+            if (isset($_POST['dataSubmit']) && empty($_POST['username']) && empty($_POST['password'])) 
+            {
+                $_SESSION['message']=  'ERREUR : veuillez remplir tous les champs !';
+                header('Location: index.php'); exit;
+            }
+        
         ?>
 
                     <!-- index html -->
@@ -57,13 +74,16 @@
         <div class="form_container">
             <fieldset>
                 <legend>Se connecter :</legend>
+                    <span class="message">
+                        <?php echo $message; ?>
+                    </span>
                     <form method="post" action="index.php">
                         <p>
                             <label for="pseudo">Identifiant : </label>
-                            <input type="text" id="pseudo" name="username" required/>
+                            <input type="text" id="pseudo" name="username"/>
 
                             <label for="mp">Mot de passe : </label>
-                            <input type="password" id="mp" name="password" required/>
+                            <input type="password" id="mp" name="password"/>
 
                             <input class="button-envoyer" type="submit" name="dataSubmit" value="Connexion"/>
                                         
