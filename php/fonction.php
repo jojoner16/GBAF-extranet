456bbd1e2f6daefee6fb7f08c6d32dcda6548df0
Laybats Jonathan <laybats.jonathan@orange.fr>
2021-09-29 12:12:47 +0200

    Ajout du dossier php avec le fichier fonction pour la connection à la bdd

---
+++ php/fonction.php
@@ -0,0 +1,28 @@
+<?php
+/*
+*   Fichier de fonctions génériques
+*   - connect_bdd()
+*       Permet de centraliser la connexion à MySQL, les infos de connexion
+*       se trouvent ici (login/mot de passe, nom de la BDD).
+*       Renvoie un objet PDO avec lequel on peut exécuter des requêtes.
+*/
+function connect_bdd()
+{
+    // connexion à la BDD
+	try
+	{
+		$pdo_options = array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+            PDO::ATTR_ERRMODE =>  PDO::ERRMODE_EXCEPTION
+        );
+		$bdd = new PDO('mysql:host=localhost;dbname=gbaf', 'root', '', $pdo_options);
+		return $bdd;
+	}
+	catch(Exception $e)
+	{
+		die('Erreur : '.$e->getMessage());
+	}
+    
+}
+
+?>
