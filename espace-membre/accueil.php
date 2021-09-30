82db1c87b7ee750b885bdcc193ad114f99dca7d4
Laybats Jonathan <laybats.jonathan@orange.fr>
2021-09-30 16:00:37 +0200

    dossier espace-membre et ajout de la page accueil

---
+++ espace-membre/accueil.php
@@ -0,0 +1,126 @@
+<?php
+    session_start();
+    require_once '../php/fonction.php';
+    $pdo = connect_bdd();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/jpg" href="images/logo.png" />
+        <link rel="stylesheet" type="text/css" href="/styles.css">
+        <title>GBAF accueil</title>
+    </head>
+    
+    <body>
+        <?php
+            require_once '../header-footer/header.php';
+
+            // REDIRECTION: NON CONNECTÉ
+            if (!isset($_SESSION['nom']) && !isset($_SESSION['prenom']) && !isset($_SESSION['id_user'])) 
+            {
+                header('Location: index.php');
+                exit();
+            }
+
+            // fonction recherche les infos des acteurs dans la bdd
+            function searchActeurs($pdo)
+            {
+                $req_data_acteur = $pdo->prepare('SELECT *, SUBSTR(description, 1, 135) AS firstLineDescription FROM acteur');
+                $req_data_acteur->execute();
+
+                while ($dataPartenaires = $req_data_acteur->fetch()) 
+                {
+                    echo '<li class="acteur_seul">';
+                    echo '<img src="../images/'.$dataPartenaires['logo'].'">';
+                    echo '<h3>' . $dataPartenaires['acteur'] . '</h3>';
+                    echo '<div class="acteur-seul_description"><p>' . $dataPartenaires['firstLineDescription'] . ' (...)</p></div>';
+                    echo '<a href="/espace-membre/partenaire.php?id_acteur=' . $dataPartenaires['id_acteur'] . ' ">Lire la suite</a> ';
+                    echo '</li>';
+                }
+                $req_data_acteur->closeCursor();
+            }
+        ?>
+        
+                  <!-- Page accueil -->
+        <main>
+        
+            <!-- Section Présentation GBAF -->
+            <section class="GBAF">
+                <h1>GBAF (Groupement Banque Assurance Français)</h1>
+
+                <div class="text">
+                    <p>
+                        <strong>
+                            Le Groupement Banque Assurance Français (GBAF)
+                        </strong>
+                        est une fédération représentant les 6 grands groupes
+                        français :
+                    </p>
+                    <ul>
+                        <li>BNP Paribas</li>
+                        <li>BPCE</li>
+                        <li>Crédit Agricole</li>
+                        <li>Crédit Mutuel-CIC</li>
+                        <li>Société Générale</li>
+                        <li>La Banque Postale</li>
+                    </ul>
+                    <p>
+                        Même s’il existe une forte concurrence entre ces
+                        entités, elles vont toutes travailler de la même façon
+                        pour gérer près de 80 millions de comptes sur le
+                        territoire national. <br />Le GBAF est le représentant
+                        de la profession bancaire et des assureurs sur tous  les
+                        axes de la réglementation financière française. Sa
+                        mission est de promouvoir l'activité bancaire à
+                        l’échelle nationale. C’est aussi un interlocuteur
+                        privilégié des pouvoirs publics.
+                    </p>
+                    <br />
+                    <p>
+                        Les produits et services bancaires sont nombreux et très
+                        variés. Afin de renseigner au mieux les clients, les
+                        salariés des 340 agences des banques et assurances en
+                        France (agents, chargés de clientèle, conseillers
+                        financiers, etc.) recherchent sur Internet des
+                        informations portant sur des produits bancaires et des
+                        financeurs, entre autres. <br />Aujourd’hui, il n’existe
+                        pas de base de données pour chercher ces informations
+                        de manière fiable et rapide ou pour donner son avis sur
+                        les partenaires et acteurs du secteur bancaire, tels que
+                        les associations ou les financeurs solidaires. Pour
+                        remédier à cela, le GBAF souhaite proposer aux salariés
+                        des grands groupes français un point d’entrée unique,
+                        répertoriant un grand nombre d’informations sur les
+                        partenaires et acteurs du groupe ainsi que sur les
+                        produits et services  bancaires et financiers.
+                        <br />Chaque salarié pourra ainsi poster un commentaire
+                        et donner son avis.
+                    </p>
+                </div>
+
+                <div class="GBAF-illustration">
+                    <img src="../images/logo.png" alt="Logo GBAF" />
+                </div>
+            </section>
+
+            <!-- Section présentation Acteurs / Partenaires -->
+            <section class="acteurs">
+                <h2>Acteurs et partenaires du système bancaire français</h2>
+
+                <p class="text">texte acteurs et partenaires</p>
+
+                <nav class="acteurs_list">
+                        <ul>
+                            <!-- <li> acteur_seul-->
+                            <?php searchActeurs($pdo); ?>
+                        </ul>  
+                </nav>
+            </section>
+        </main>
+   
+        <?php require_once '../header-footer/footer.php'?>
+    </body>
+</html>
