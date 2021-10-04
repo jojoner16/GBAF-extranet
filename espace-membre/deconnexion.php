20f230feb1fdcca0dde327bad37083861ba9eb1d
Laybats Jonathan <laybats.jonathan@orange.fr>
2021-10-04 13:00:22 +0200

    Crétiona de la page déconnexion

---
+++ espace-membre/deconnexion.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!isset($_GET['redirection'])) 
+{
+    
+    header('Location: mp.php');
+    exit();
+}
+
+require_once('../php/account.php');
+
+if (isset($_GET['redirection']) && $_GET['redirection'] == 'mp') 
+{
+    deleteSession();
+
+    header('Location: ../mp.php');
+    exit();
+}
+
+if (isset($_GET['redirection']) && $_GET['redirection'] == 'exit') 
+{
+    deleteSession();
+
+    header('Location: ../index.php');
+    exit();
+}
