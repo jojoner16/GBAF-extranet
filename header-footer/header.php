61ec4898c101ab362571047a7440f2b9d82d3af7
Laybats Jonathan <laybats.jonathan@orange.fr>
2021-10-04 13:08:09 +0200

    modification synthax et header

--- header-footer/header.php
+++ header-footer/header.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
+        <meta charset="utf-8" />
 		<link rel="stylesheet" href="../styles.css" />
         <link rel="icon" type="image/jpg" href="../images/logo.png" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;900&display=swap"	rel="stylesheet"/>
         <!-- <title>GBAF extranet</title> -->
 	</head>
 
-	
+	<body>
         <div class="contain_all">
             <header>
 				<!-- Logo GBAF -->
 				<a class="header-logo" href="espace-membre/accueil.php">
 					<img id="logo" src="../images/logo.png" alt="Logo GBAF" />
 				</a>
-
+                
                 <?php // affiche le profil uniquement si un utilsateur est connecté
                 if (isset($_SESSION['nom']) && isset($_SESSION['prenom']) && isset($_SESSION['id_user'])) 
                 {
@@ -23,19 +25,18 @@
                 <ul class="header-profil">
                     <!-- Nom Prenom -->
                     <li class="profil_nom">
-                        <img id="co" src="../images/login.jpg"/>
                         <p> <?php echo htmlspecialchars($_SESSION['nom']); ?> </p>
                         <p> <?php echo htmlspecialchars($_SESSION['prenom']); ?> </p>
                     </li>
 
                     <!-- bouton Déconnexion -->
                     <li>
-                        <a href="/espace-membre/deconnexion.php?redirection=exit">Se déconnecter</a>
+                        <a href="../espace-membre/deconnexion.php?redirection=exit">Se déconnecter</a>
                     </li>
 
                     <!-- bouton Modifier son Profil -->
                     <li>
-                        <a href="/espace-membre/parametres-compte.php">Paramètres du compte</a>
+                        <a href="../espace-membre/parametres-compte.php">Paramètres du compte</a>
                     </li>
                 </ul>
                 <?php
