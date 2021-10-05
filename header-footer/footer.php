966d0b8a47dc79047bfaa840274921edc804ff74
Laybats Jonathan <laybats.jonathan@orange.fr>
2021-10-05 10:25:00 +0200

    modification de la page footer

--- header-footer/footer.php
+++ header-footer/footer.php
@@ -2,8 +2,6 @@
 <html lang="fr">
 	<head>
 		<link rel="stylesheet" href="../styles.css" />
-        <link rel="icon" type="image/jpg" href="../images/logo.png" />
-        <!-- <title>GBAF extranet</title> -->
 	</head>
     <body> 
         <footer>
