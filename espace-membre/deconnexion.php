b55bbdc0c72f459467bb408c6ca8fddfa137169c
Laybats Jonathan <laybats.jonathan@orange.fr>
2021-10-05 12:37:29 +0200

    erreur chemins d'accès->correction

--- espace-membre/deconnexion.php
+++ espace-membre/deconnexion.php
@@ -1,26 +1,27 @@
 <?php
 
-if (!isset($_GET['redirection'])) 
-{
-    
-    header('Location: mp.php');
-    exit();
-}
+    if (!isset($_GET['redirection'])) 
+    {
+        
+        header('Location: ../mp.php');
+        exit();
+    }
 
-require_once('../php/account.php');
+    require_once('../php/account.php');
 
-if (isset($_GET['redirection']) && $_GET['redirection'] == 'mp') 
-{
-    deleteSession();
+    if (isset($_GET['redirection']) && $_GET['redirection'] == 'mp') 
+    {
+        deleteSession();
 
-    header('Location: ../mp.php');
-    exit();
-}
+        header('Location: ../mp.php');
+        exit();
+    }
 
-if (isset($_GET['redirection']) && $_GET['redirection'] == 'exit') 
-{
-    deleteSession();
+    if (isset($_GET['redirection']) && $_GET['redirection'] == 'exit') 
+    {
+        deleteSession();
 
-    header('Location: ../index.php');
-    exit();
-}
+        header('Location: ../index.php');
+        exit();
+    }
+?>
