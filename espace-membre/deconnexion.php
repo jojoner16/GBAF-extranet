931a1b56da3b85b7d3c73aae3436dd9d11b0fcd3
Laybats Jonathan <laybats.jonathan@orange.fr>
2021-10-19 11:57:31 +0200

    Réorganisation du code, suppression espaces inutiles

--- espace-membre/deconnexion.php
+++ espace-membre/deconnexion.php
@@ -2,25 +2,21 @@
 
     if (!isset($_GET['redirection'])) 
     {
-        
         header('Location: ../mp.php');
         exit();
     }
-
+    
     require_once('../php/account.php');
 
     if (isset($_GET['redirection']) && $_GET['redirection'] == 'mp') 
     {
         deleteSession();
-
         header('Location: ../mp.php');
         exit();
     }
-
     if (isset($_GET['redirection']) && $_GET['redirection'] == 'exit') 
     {
         deleteSession();
-
         header('Location: ../index.php');
         exit();
     }
